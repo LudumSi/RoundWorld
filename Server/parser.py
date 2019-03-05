c87c2acbb8fb1fca88d69215b2b09e8c194ef7c1
Benjamin Green <benjaminisaacgreen@gmail.com>
2019-03-05 00:38:42 -0800

    Fixed Parser
    Parser now works completely. Need to add more functionality for incomplete messages/data. Also, the amount of component values that can be sent is a max of 4 atm since the array size must be defined. However, this can be changed if we decide we need a lot of values for a component. It might take longer to process.

--- Server/parser.py
+++ Server/parser.py
@@ -7,38 +7,55 @@ def generate():
 def parse(data):
 	
 	start = 0
-	component_num = 0
+	component_num = -1
 	val_num = 0
 	command_id = ""
 	ip = []
 	length = 0
 	components = []
-	component_vals = [[]]
+	component_vals = []
 
-	for i,str_i_think in enumerate(data):
-		start = 0
+	for i in range(len(data)):
 		if data[i] == "L":
-			length = int(data[start: i - 1]) #<length>
+			length = int(data[0: i]) #<length>
 			start = i + 1
-		elif data[i] == "{":
-			command_id = data[start: start + 3] #<command_id
-			start = i + 2
-		elif data[i] == "(": #
-			components.append(data[start: start + 3]) #command_args
-			component_num += 1
+			print("Getting Length")
 
-	for x in range(start, len(data)): #command
-		if data[x] == ")":
-			ip[component_num] = data[start + 1: x - 1]
-			break;
-		elif data[x] == ":":
-			start = x
-		elif data[x] == ",":
-			val_num += 1
-			component_vals[component_num][val_num] = data[start + 1: x - 1]
-			start = x
-		elif data[x] == "|":
-			start = x
+		elif data[i] == "{":
+			command_id = data[i - 4: i] #<command_id
+			start = i + 2
+			print("Getting Command")
+			
+		elif data[i] == "(": #
+			components.append(data[start: start + 4]) #command_args
+			component_vals.append([0] * 4) #needs to be variable for different numbers of values
+			component_num += 1
+			start = i
+			val_num = 0
+			print("Getting Component")
+
+
+			for x in range(start, len(data)): #command
+				print(start)
+				if data[x] == ")":
+					ip.append(data[start + 1: x])
+					start = x + 2
+					print("End of Component")
+					break;
+				elif data[x] == ":":
+					print("Getting First Comp_val")
+					start = x
+				elif data[x] == ",":
+					print("Getting Comp_Val")
+					print(data[start + 1: x])
+					component_vals[component_num][val_num] = data[start + 1: x]
+					val_num += 1
+					start = x
+				elif data[x] == "|":
+					component_vals[component_num][val_num] = data[start + 1: x]
+					start = x
+					print("Getting IP")
+					start = x
 	
 	print(length)
 	print(command_id)
@@ -46,10 +63,8 @@ def parse(data):
 	print(component_vals)
 	print(ip)
 
-parse("1L0101{(2020:efewfew,fewfefw|1111)(4040:efewfew,3fee|2222)}")
+parse("10L0101{(2020:val1,val2|123.456.789)(4040:val3,val4,val5,val6|222.222.789)(4040:val3,val4|222.222.789)}")
 
-
-            #command_name = data[i+1:i+4]
         
     #<length>Lxxxx{(xxxx:text|<ip>)}
     #<length>L<command_id>{(<component_args>:<component_vals>)}
