39b70a2dacde099a435b05b502f7bd9ee9a75ba5
LudumSi <Neomaster2300@gmail.com>
2019-05-27 15:28:51 -0700

    Implemented control loop

--- Server/main_loop.py
+++ Server/main_loop.py
@@ -3,18 +3,20 @@ from threading import Thread
 
 class MainLoopThread(Thread):
 	
-	def __init__(self, queue):
+	def __init__(self, queue, control):
 	
 		Thread.__init__(self)
 		self.queue = queue
-		self.running = True
-		print("Started the main game loop")
+		self.control = control
 	
 	def run(self):
 		
 		#Game loop
 		print("Started Main Loop Thread\n")
-		while self.running:
+		while self.control.running:
+				pass
+		
+		print("Closing Main Loop Thread\n")
 
 		
 #length|id:{key:val,key:val,key:val}		
